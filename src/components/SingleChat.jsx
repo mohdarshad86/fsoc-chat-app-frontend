bcc3cdf2d0bedf2472d68d4fd5658a8cdcbc81a5
Mohd Arshad <mohdarshad8668@gmail.com>
2023-04-14 03:53:20 +0530

    added build folder

--- src/components/SingleChat.jsx
+++ src/components/SingleChat.jsx
@@ -15,7 +15,8 @@ import animationData from "../Animations/typing.json";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./Miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT = "http://localhost:3001";
+// const ENDPOINT = "http://localhost:3001";
+const ENDPOINT = "https://chatappbackend-uqkv.onrender.com";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
